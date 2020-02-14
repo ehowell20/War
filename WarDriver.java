9280043ba364ca367a1ca054cf0601a9a22793c3
Erika <ehowell20@punahou.edu>
2020-02-14 18:57:23 +0000

    play game (no slap)

--- WarDriver.java
+++ WarDriver.java
@@ -1,17 +1,51 @@
+import java.util.Scanner;
+
 public class WarDriver
 {
     public static void main(String[] args)
     {
         int card = 0;
+        // player has half deck
+        int playerCards = 26;
+        // computer has half deck
+        int compCards = 26;
+        boolean hasWon = false;
         // creates new deck of cards
         Cards deck = new Cards();
         // shuffles deck
         deck.shuffleCards();
-        // picks a random card
-        card = deck.pickCard();
-        // if card value is 1, 11, 12, or 13 show face letter (A, J, Q, K)
+        while (!hasWon)
+        {
+            // player turn
+            pause();
+            // picks a random card
+            card = deck.pickCard();
+            // print card
+            printCard(card);
+            // slap?
+            // computer turn
+            // picks a random card
+            card = deck.pickCard();
+            // print card
+            printCard(card);
+            // slap?
+        }
+    }
+    public static void pause()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Press any key to place a card");
+        in.nextLine();
+    }
+    public static void printCard(int card)
+    {
         switch (card)
         {
+            // if card value is 1, 11, 12, or 13 show face letter (A, J, Q, K)
+            default:
+                // print out original card value
+                System.out.println(card);
+                break;
             case 1:
                 System.out.println("A");
                 break;
@@ -23,10 +57,6 @@ public class WarDriver
                 break;
             case 13:
                 System.out.println("K");
-                break;
-            default:
-                // print out original card value
-                System.out.println(card);
         }
     }
 }
