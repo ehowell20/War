0e3126b2d97e10c0fce2eb51ce59d2845f676129
Erika <ehowell20@punahou.edu>
2020-02-25 18:52:34 +0000

    mark if royal and chances, began steps after royal card played

--- WarDriver.java
+++ WarDriver.java
@@ -7,9 +7,16 @@ public class WarDriver
         int card = 0;
         // player has half deck
         int playerCards = 26;
+        // player won cards
+        int playerCardsWon = 0;
         // computer has half deck
         int compCards = 26;
+        // computer won cards
+        int compCardsWon = 0;
+        // number of cards player can put down to try and put down a royal
+        int chances = 0;
         boolean hasWon = false;
+        boolean isRoyal = false;
         // creates new deck of cards
         Cards deck = new Cards();
         // shuffles deck
@@ -21,14 +28,81 @@ public class WarDriver
             // picks a random card
             card = deck.pickCard();
             // print card
-            printCard(card);
-            // slap?
+            switch (card)
+            {
+                // if card value is 1, 11, 12, or 13 show face letter (A, J, Q, K)
+                // if royal, mark isRoyal as true
+                // if royal, change chances value to the number of tries other player has
+                default:
+                    // print out original card value
+                    System.out.println(card);
+                    isRoyal = false;
+                    break;
+                case 1:
+                    System.out.println("A");
+                    isRoyal = true;
+                    chances = 4;
+                    break;
+                case 11:
+                    System.out.println("J");
+                    isRoyal = true;
+                    chances = 1;
+                    break;
+                case 12:
+                    System.out.println("Q");
+                    isRoyal = true;
+                    chances = 2;
+                    break;
+                case 13:
+                    System.out.println("K");
+                    isRoyal = true;
+                    chances = 3;
+            }
             // computer turn
             // picks a random card
             card = deck.pickCard();
             // print card
-            printCard(card);
-            // slap?
+            switch (card)
+            {
+                // if card value is 1, 11, 12, or 13 show face letter (A, J, Q, K)
+                // if royal, mark isRoyal as true
+                // if royal, change chances value to the number of tries other player has
+                default:
+                    // print out original card value
+                    System.out.println(card);
+                    isRoyal = false;
+                    break;
+                case 1:
+                    System.out.println("A");
+                    isRoyal = true;
+                    chances = 4;
+                    break;
+                case 11:
+                    System.out.println("J");
+                    isRoyal = true;
+                    chances = 1;
+                    break;
+                case 12:
+                    System.out.println("Q");
+                    isRoyal = true;
+                    chances = 2;
+                    break;
+                case 13:
+                    System.out.println("K");
+                    isRoyal = true;
+                    chances = 3;
+            }
+            // if royal, human plays cards
+            if (isRoyal)
+            {
+                // human places down cards until hits limit or plays royal
+                for (int i = 0; i < chances; i++)
+                {
+                    pause();
+                    // if royal, computer places down cards until hits limit or plays royal
+                    // if no royals and chances up, stack count gets added to computer points
+                }
+            }
         }
     }
     public static void pause()
@@ -36,27 +110,5 @@ public class WarDriver
         Scanner in = new Scanner(System.in);
         System.out.println("Press any key to place a card");
         in.nextLine();
-    }
-    public static void printCard(int card)
-    {
-        switch (card)
-        {
-            // if card value is 1, 11, 12, or 13 show face letter (A, J, Q, K)
-            default:
-                // print out original card value
-                System.out.println(card);
-                break;
-            case 1:
-                System.out.println("A");
-                break;
-            case 11:
-                System.out.println("J");
-                break;
-            case 12:
-                System.out.println("Q");
-                break;
-            case 13:
-                System.out.println("K");
-        }
     }
 }
